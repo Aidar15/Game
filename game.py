2bbc8776698d0163fe3a6a348239eb2f76408a26
Aidar <aidvildanov@yandex.ru>
2019-12-28 16:16:27 +0300

    Second_commit

--- game.py
+++ game.py
@@ -2,16 +2,63 @@ import pygame
 import sys
 import os
 
-size = weight, height = 600, 600
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    image = pygame.image.load(fullname).convert()
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+size = width, height = 1280, 720
 screen = pygame.display.set_mode(size)
 fps = 60
 clock = pygame.time.Clock()
 
-running = True
-while running:
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["Press 'tab' to start"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
+    screen.blit(fon, (0, 0))
+    pygame.init()
+    font = pygame.font.Font(None, 50)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('grey'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 570
+        intro_rect.top = text_coord
+        intro_rect.x = 500
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_CAPSLOCK:
+                    return
+        pygame.display.flip()
+        clock.tick(fps)
+
+
+start_screen()
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
-    screen.fill((0, 0, 0))
+            terminate()
     pygame.display.flip()
     clock.tick(fps)
