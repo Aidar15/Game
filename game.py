e716bda97a6ef7cc3415f6808686edd1038f0ca4
Aidar <aidvildanov@yandex.ru>
2020-01-02 17:03:02 +0300

    demo_version

--- game.py
+++ game.py
@@ -15,10 +15,13 @@ def load_image(name, colorkey=None):
     return image
 
 
+pygame.init()
 size = width, height = 1280, 720
 screen = pygame.display.set_mode(size)
-fps = 60
+screen.fill((255, 255, 255))
+fps = 30
 clock = pygame.time.Clock()
+running = True
 
 
 def terminate():
@@ -48,17 +51,111 @@ def start_screen():
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_CAPSLOCK:
-                    return
+                if event.key == 32:
+                    return  # начинаем игру
         pygame.display.flip()
         clock.tick(fps)
 
 
 start_screen()
 
-while True:
+
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+boxes = pygame.sprite.Group()
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(tile_width_v * pos_x, tile_height_v * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(tile_width_v * pos_x + 15, tile_height_v * pos_y + 5)
+
+    def move(self):
+        for sprite in all_sprites:
+            camera.apply(sprite)
+
+
+class Camera:
+    # зададим начальный сдвиг камеры
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    # сдвинуть объект obj на смещение камеры
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    # позиционировать камеру на объекте target
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
+
+
+tile_images = {'wall': load_image('grey_2.png'), 'empty': load_image('white.png')}
+player_image = load_image('mar.png')
+tile_width_v, tile_height_v = 32, 32
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
+
+
+player, level_x, level_y = generate_level(load_level('game_map.txt'))
+
+camera = Camera()
+camera.update(player)
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            terminate()
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pass
+        if event.type == pygame.KEYDOWN:
+            if event.key == 97:
+                player.rect.x -= 40
+            elif event.key == 100:
+                player.rect.x += 40
+            elif event.key == 273:
+                player.rect.y -= 40
+            elif event.key == 274:
+                player.rect.y += 40
+    screen.fill((255, 255, 255))
+    all_sprites.draw(screen)
+    tiles_group.draw(screen)
+    player_group.draw(screen)
     pygame.display.flip()
     clock.tick(fps)
